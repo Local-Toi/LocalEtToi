a37dde7c38e4c9e7d747eab8704bc2dfaf120669
Adrian Rosin <adrian.rosin.etu@univ-lille.fr>
2023-11-20 15:46:39 +0100

    feat: added mainview + tentative d'ombre circulaire foireux

--- lib/views/first.dart
+++ lib/views/first.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:local_et_toi/views/sign_in.dart';
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
@@ -18,7 +19,55 @@ class MainView extends StatelessWidget {
             Positioned(
               left: 44,
               top: 107,
+              width: 300,
+              height: 300,
               child: Image.asset("assets/images/logo1.png"),
+
+            ),
+            Positioned(
+              left: 46,
+              top: 516,
+              child: SizedBox(
+                width: 300,
+                height: 40,
+                child: Material(
+                  color: Colors.transparent,
+                  elevation: 100, // Ajustez cela pour le niveau d'ombre
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: const Color(0xFF095D40),
+                        padding: EdgeInsets.zero,
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) => const LoginPage(),
+                          ),
+                        );
+                      },
+                      child: const SizedBox(
+                        width: 300,
+                        child: Text(
+                          'Se connecter',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w400,
+                            height: 0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
 
             )
           ]
