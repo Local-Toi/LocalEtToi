e128abc3ff71e10a3f1f3c9196b788b2d3061cff
chachignot <noe.chachignot.etu@univ-lille.fr>
2023-11-21 15:09:20 +0100

    style(utils): colors constants update
    update constants.dart with other colors constant for the app, remove dark theme colors because no time for it in the planning.

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 const Color darkGreen = Color(0xFF095D40);
 
-const Color lightGreen = Color(0xFF40B65D);
+const Color darkGreen50 = Color(0x80095D40);
+
+const Color lightGreen = Color(0xFFE2F1CE);
 
 const Color beige = Color(0xFFFFFBE2);
 
@@ -10,8 +12,10 @@ const Color red = Color(0xFFA30000);
 
 const Color darkBrown = Color(0xFF8E6E34);
 
-const Color lightBrown = Color(0xFFC19544);
+const Color grey100 = Color(0xFF333333);
 
 const Color grey85 = Color(0xD9333333);
 
 const Color grey50 = Color(0x80333333);
+
+const Color taupe = Color(0xFF7D7171);
