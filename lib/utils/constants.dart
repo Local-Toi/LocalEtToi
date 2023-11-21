439f046b593a0ea4a58a95e53ddf8e5a70b72ce7
chachignot <noe.chachignot.etu@univ-lille.fr>
2023-11-21 14:58:49 +0100

    style(utils): colors constants
    Add constants.dart with color constant for the app.

---
+++ lib/utils/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const Color darkGreen = Color(0xFF095D40);
+
+const Color lightGreen = Color(0xFF40B65D);
+
+const Color beige = Color(0xFFFFFBE2);
+
+const Color red = Color(0xFFA30000);
+
+const Color darkBrown = Color(0xFF8E6E34);
+
+const Color lightBrown = Color(0xFFC19544);
+
+const Color grey85 = Color(0xD9333333);
+
+const Color grey50 = Color(0x80333333);
