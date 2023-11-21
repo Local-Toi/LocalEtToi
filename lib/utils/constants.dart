04f80bc9cf6d0d3be39005209c6e5ea758148110
chachignot <noe.chachignot.etu@univ-lille.fr>
2023-11-21 16:28:45 +0100

    style(utils): new constants
    Add fonts constants to constants.dart to not have to create a TexteStyle everytime. Add two more colors.

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// Colors constant used for the entire app
+
 const Color darkGreen = Color(0xFF095D40);
 
 const Color darkGreen50 = Color(0x80095D40);
@@ -19,3 +21,17 @@ const Color grey85 = Color(0xD9333333);
 const Color grey50 = Color(0x80333333);
 
 const Color taupe = Color(0xFF7D7171);
+
+// Fonts constant used for the entire app
+
+const TextStyle logo = TextStyle(fontFamily: 'CinzelDecorative', fontSize: 50);
+
+const TextStyle titre = TextStyle(fontFamily: 'Montserrat', fontSize: 24);
+
+const TextStyle textBig = TextStyle(fontFamily: 'Montserrat', fontSize: 20);
+
+const TextStyle text = TextStyle(fontFamily: 'Montserrat', fontSize: 18);
+
+const TextStyle textMedium  = TextStyle(fontFamily: 'Montserrat', fontSize: 14);
+
+const TextStyle textSmall = TextStyle(fontFamily: 'Montserrat', fontSize: 8);
