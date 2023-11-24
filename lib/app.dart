e59923d031969f2cc692b11df841da4240826cbb
Justin <justinsillou@gmail.com>
2023-11-24 10:48:10 +0100

    Update package imports + test implementation

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,10 @@
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_login/authentication/authentication.dart';
-import 'package:flutter_login/home/home.dart';
-import 'package:flutter_login/login/login.dart';
-import 'package:flutter_login/splash/splash.dart';
+import 'package:local_et_toi/authentication/authentication.dart';
+import 'package:local_et_toi/home/home.dart';
+import 'package:local_et_toi/login/login.dart';
+import 'package:local_et_toi/splash/splash.dart';
 import 'package:user_repository/user_repository.dart';
 
 class App extends StatefulWidget {
