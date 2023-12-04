03eabb0fd80246edda088561cbb16b3688de30c9
Gaelle Fret <gaelle.fret.etu@univ-lille.fr>
2023-12-04 16:43:49 +0100

    refactor: change GreenRoundedButton
    Replacement of the hard coded values with the constants in utils/constants

--- lib/utils/buttons/buttons.dart
+++ lib/utils/buttons/buttons.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:local_et_toi/utils/constants.dart' as constants;
 
 class GreenRoundedButton extends StatelessWidget {
   final VoidCallback onPressed;
@@ -13,41 +14,22 @@ class GreenRoundedButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 300,
-      height: 40,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: const [
-          BoxShadow(
-            color: Color(0x3F000000),
-            blurRadius: 4,
-            offset: Offset(0, 4),
-            spreadRadius: 0,
-          ),
-        ],
-      ),
+      width: MediaQuery.of(context).size.width / 1.3,
+      height: MediaQuery.of(context).size.height /20.6,
       child: TextButton(
         onPressed: onPressed,
         style: TextButton.styleFrom(
-          backgroundColor: const Color(0xFF095D40),
-          padding: EdgeInsets.zero,
+          backgroundColor: constants.darkGreen,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20),
+            borderRadius: BorderRadius.circular(50),
           ),
         ),
         child: SizedBox(
-          width: 300,
           child: Center(
             child: Text(
               buttonText,
               textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontFamily: 'Montserrat',
-                fontWeight: FontWeight.w400,
-                height: 0,
-              ),
+              style: constants.textBeige,
             ),
           ),
         ),
@@ -68,39 +50,12 @@ class TransparentRoundedButtonWithBorder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 300,
-      height: 40,
-      decoration: BoxDecoration(
-        border: Border.all(
-          width: 3,
-          color: const Color(0xFF095D40),
-        ),
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: TextButton(
-        onPressed: onPressed,
-        style: TextButton.styleFrom(
-          padding: EdgeInsets.zero,
-          backgroundColor: Colors.transparent,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30),
-          ),
-        ),
-        child: SizedBox(
-          width: 300,
-          child: Center(
-            child: Text(
-              buttonText,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Color(0xFF095D40),
-                fontSize: 18,
-                fontFamily: 'Montserrat',
-                fontWeight: FontWeight.w400,
-                height: 0,
-              ),
-            ),
+    return TextButton(
+      onPressed: onPressed,
+      child: SizedBox(
+        child: Center(
+          child: Text(
+            buttonText,
           ),
         ),
       ),
