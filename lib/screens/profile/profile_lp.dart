45f386421406432c1b34fe56427057c42c1c96c8
Justin <justin.sillou.etu@univ-lille.fr>
2023-12-04 17:19:44 +0100

    fix: logout ok

--- lib/screens/profile/profile_lp.dart
+++ lib/screens/profile/profile_lp.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:local_et_toi/blocs/authentication_bloc/authentication_bloc.dart';
 import 'package:local_et_toi/blocs/sign_in_bloc/sign_in_bloc.dart';
+import 'package:local_et_toi/screens/home/home_screen.dart';
 import 'package:user_repository/user_repository.dart';
 
 class ProfileLP extends StatelessWidget {
@@ -16,6 +17,11 @@ class ProfileLP extends StatelessWidget {
         onPressed: () {
           print("logout");
           context.read<SignInBloc>().add(const SignOutRequired());
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+              builder: (context) => const HomeScreen(),
+            ),
+          );
         },
         child: const Text('Logout'),
       ),
