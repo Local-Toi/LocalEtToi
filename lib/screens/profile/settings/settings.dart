4655796e71ce0ead6fb35481e0f7dd179c5baf48
chachignot <noe.chachignot.etu@univ-lille.fr>
2023-12-11 17:34:52 +0100

    refactor : change directory of most profile linked views
    Move settings views to a settings directory

--- lib/screens/profile/settings/settings.dart
+++ lib/screens/profile/settings/settings.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:local_et_toi/utils/constants.dart' as constants;
 import 'package:local_et_toi/utils/buttons/buttons.dart';
-import 'package:local_et_toi/screens/profile/Security.dart';
-import 'package:local_et_toi/screens/profile/about.dart';
-import 'package:local_et_toi/screens/profile/assistance.dart';
-import 'package:local_et_toi/screens/profile/cgu.dart';
+import 'Security.dart';
+import 'about.dart';
+import 'assistance.dart';
+import 'cgu.dart';
 
 void main()  {
   runApp(const MaterialApp(
