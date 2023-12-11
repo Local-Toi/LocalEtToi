62057b84444d93ef9d36950a90a13d1bc8dbc583
Gaelle Fret <gaelle.fret.etu@univ-lille.fr>
2023-12-11 17:42:15 +0100

    Refactor : WIP create a dictionary to refactor all the views
    Beginning of the creation of the french dictionary for all the texts displayed in the views (label, error messages, etc) : forgot_password.dart done

---
+++ lib/utils/dictionary.dart
@@ -0,0 +1,12 @@
+Map<String, String> dict_fr = {
+  //forgot_password.dart
+  'forgotten_pwd' : 'Mot de passe oublié',
+  'recup_link' : 'Saisis ton adresse email pour obtenir un lien de récupération',
+  'email' : 'Adresse email',
+  'enter_email' : 'Veuillez entrer une adresse email',
+  'send_link' : 'Envoyer le lien',
+
+  //sign-in.dart
+  'connection' : 'Connexion',
+
+};
