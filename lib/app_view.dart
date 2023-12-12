1fccb7260e7bd20ebb5fad894c2600f77dea5d89
Justin <justin.sillou.etu@univ-lille.fr>
2023-12-12 15:55:45 +0100

    feat: wip, tuto flutter lists

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:local_et_toi/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:local_et_toi/screens/loading.dart';
 import 'package:local_et_toi/screens/navigation.dart';
+import 'package:local_et_toi/utils/constants.dart' as constants;
 import 'package:user_repository/user_repository.dart';
 
 import 'screens/home/home_screen.dart';
