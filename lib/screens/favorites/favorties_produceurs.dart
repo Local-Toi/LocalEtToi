0a71b90a89b1005af58ec3a45a01564186d3a28f
Justin <justin.sillou.etu@univ-lille.fr>
2023-12-12 14:31:19 +0100

    feat: add screens favorites products and favorites producers

---
+++ lib/screens/favorites/favorties_produceurs.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FavoritesProduceurs extends StatelessWidget {
+  const FavoritesProduceurs({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/favorites/favorties_products.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FavortiesProducts extends StatelessWidget {
+  const FavortiesProducts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
