cca8fd67985738a5ec2f00c8d6bb5bffcabcf5e9
Justin <justin.sillou.etu@univ-lille.fr>
2023-12-12 15:55:45 +0100

    feat: wip, tuto flutter lists

--- lib/screens/favorites/favorties_products.dart
+++ lib/screens/favorites/favorties_products.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:local_et_toi/utils/constants.dart' as constants;
 
 class FavortiesProducts extends StatelessWidget {
   const FavortiesProducts({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ItemWidget(text: 'Item 1'),
+                  ItemWidget(text: 'Item 2'),
+                  ItemWidget(text: 'Item 3'),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({
+    super.key,
+    required this.text,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: SizedBox(
+        height: 100,
+        child: Center(child: Text(text)),
+      ),
+    );
   }
 }
