16f3463f2647687433b454dfbbfee6df9ba864d6
Justin <justin.sillou.etu@univ-lille.fr>
2023-12-16 00:00:05 +0100

    feat: add favorites_repository skeleton

---
+++ packages/favorites_repository/lib/src/firebase_favorite_repository.dart
@@ -0,0 +1,27 @@
+import '../favorite_repository.dart';
+
+class FirebaseFavoriteRepository implements FavoriteRepository {
+  @override
+  Future<void> addFavorite(String id) {
+    // TODO: implement addFavorite
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<String>> getFavorites() {
+    // TODO: implement getFavorites
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> isFavorite(String id) {
+    // TODO: implement isFavorite
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> removeFavorite(String id) {
+    // TODO: implement removeFavorite
+    throw UnimplementedError();
+  }
+}
---
+++ packages/favorites_repository/lib/src/models/my_favorites.dart
@@ -0,0 +1,9 @@
+import 'package:equatable/equatable.dart';
+
+import '../entities/entities.dart';
+
+class MyFavorites extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
