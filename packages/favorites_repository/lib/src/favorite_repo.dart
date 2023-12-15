543b35aa161206b170ab50283209cb4ae73131b5
Justin <justin.sillou.etu@univ-lille.fr>
2023-12-16 00:00:05 +0100

    feat: add favorites_repository skeleton

---
+++ packages/favorites_repository/lib/src/favorite_repo.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import '../favorite_repository.dart';
+
+abstract class FavoriteRepository {
+  Future<void> addFavorite(String id);
+  Future<void> removeFavorite(String id);
+  Future<List<String>> getFavorites();
+  Future<bool> isFavorite(String id);
+}
