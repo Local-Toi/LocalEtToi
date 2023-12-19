28e9b01ce1f362b61bc49bc133e655304d365dc5
Justin <justin.sillou.etu@univ-lille.fr>
2023-12-19 10:48:02 +0100

    feat: add comments for authentication bloc

--- lib/blocs/authentication_bloc/authentication_event.dart
+++ lib/blocs/authentication_bloc/authentication_event.dart
@@ -1,5 +1,7 @@
 part of 'authentication_bloc.dart';
 
+/// Base class for authentication events.
+/// All authentication events should extend this class.
 sealed class AuthenticationEvent extends Equatable {
   const AuthenticationEvent();
 
@@ -7,10 +9,13 @@ sealed class AuthenticationEvent extends Equatable {
   List<Object> get props => [];
 }
 
+/// Event triggered when the authentication status changes.
+/// It contains the updated user information.
 class AuthenticationStatusChanged extends AuthenticationEvent {
   const AuthenticationStatusChanged(this.user);
 
   final User? user;
 }
 
+/// Event triggered when a logout is requested.
 class AuthenticationLogoutRequested extends AuthenticationEvent {}
--- lib/blocs/authentication_bloc/authentication_state.dart
+++ lib/blocs/authentication_bloc/authentication_state.dart
@@ -1,20 +1,35 @@
 part of 'authentication_bloc.dart';
 
-enum AuthenticationStatus { unknown, authenticated, unauthenticated }
+/// Represents the different authentication statuses.
+enum AuthenticationStatus {
+  /// The authentication status is unknown.
+  unknown,
 
+  /// The user is authenticated.
+  authenticated,
+
+  /// The user is unauthenticated.
+  unauthenticated
+}
+
+/// Represents the state of authentication.
 class AuthenticationState extends Equatable {
   final AuthenticationStatus status;
   final User? user;
 
+  /// Constructs an [AuthenticationState] with the given status and user.
   const AuthenticationState._({
     this.status = AuthenticationStatus.unknown,
     this.user,
   });
 
+  /// Constructs an [AuthenticationState] with the unknown status.
   const AuthenticationState.unknown() : this._();
 
+  /// Constructs an [AuthenticationState] with the authenticated status and the given user.
   const AuthenticationState.authenticated(User user) : this._(status: AuthenticationStatus.authenticated, user: user);
 
+  /// Constructs an [AuthenticationState] with the unauthenticated status.
   const AuthenticationState.unauthenticated() : this._(status: AuthenticationStatus.unauthenticated);
 
   @override
