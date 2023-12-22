0a9d7d96982e7a7f8aa6f56986592ad5e15eef34
Justin <justin.sillou.etu@univ-lille.fr>
2023-12-22 13:57:08 +0100

    feat: change pubspec, add favorites_repository to dependencies. create favorite BloCs, #38

--- lib/blocs/favorite_bloc/favorite_bloc_bloc.dart
+++ lib/blocs/favorite_bloc/favorite_bloc_bloc.dart
@@ -1,11 +1,34 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:favorites_repository/favorite_repository.dart';
 
 part 'favorite_bloc_event.dart';
 part 'favorite_bloc_state.dart';
 
 class FavoriteBlocBloc extends Bloc<FavoriteBlocEvent, FavoriteBlocState> {
-  FavoriteBlocBloc() : super(FavoriteBlocInitial()) {
-    on<FavoriteBlocEvent>((event, emit) {});
+  final FavoriteRepository _favoriteRepository;
+
+  FavoriteBlocBloc({
+    required FavoriteRepository favoriteRepository,
+  })  : _favoriteRepository = favoriteRepository,
+        super(FavoriteBlocInitial()) {
+    on<FavoriteBlocEvent>((event, emit) async {
+      if (event is FavoriteAdded) {
+        await _favoriteRepository.addFavorite(event.favorite);
+      } else if (event is FavoriteRemoved) {
+        await _favoriteRepository.removeFavorite(event.favorite);
+      }
+    });
+
+    on<FavoritesRequested>((event, emit) async {
+      try {
+        final favorites = await _favoriteRepository.getFavorites();
+        emit(FavoritesLoadSuccess(
+          favorites,
+        ));
+      } catch (e) {
+        emit(FavoritesLoadFailure());
+      }
+    });
   }
 }
--- lib/blocs/favorite_bloc/favorite_bloc_state.dart
+++ lib/blocs/favorite_bloc/favorite_bloc_state.dart
@@ -2,9 +2,38 @@ part of 'favorite_bloc_bloc.dart';
 
 sealed class FavoriteBlocState extends Equatable {
   const FavoriteBlocState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class FavoriteBlocInitial extends FavoriteBlocState {}
+
+class FavoriteAdded {
+  final String favorite;
+
+  const FavoriteAdded(this.favorite);
+}
+
+class FavoriteRemoved {
+  final String favorite;
+
+  const FavoriteRemoved(this.favorite);
+}
+
+class FavoritesLoadSuccess extends FavoriteBlocState {
+  final List<String> favorites;
+
+  const FavoritesLoadSuccess(
+    this.favorites,
+  );
+
+  @override
+  List<Object> get props => [favorites];
+}
+
+class FavoritesLoadFailure extends FavoriteBlocState {
+  final String? message;
+
+  const FavoritesLoadFailure({this.message});
+}
