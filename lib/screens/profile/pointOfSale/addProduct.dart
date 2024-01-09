d9de491e04750616aa2a30d9defd4b3a4c38e2bf
GaelleFret <gaelle.fret@gmail.com>
2024-01-09 23:57:38 +0100

    feat :  modification image picker and new controllers for the inputs
    try to upload the image on the view and put it on the db but doesn't work + add controllers for the values in the input to not be deleted when the value of a dropDownMenu is changed

--- lib/screens/profile/pointOfSale/addProduct.dart
+++ lib/screens/profile/pointOfSale/addProduct.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:local_et_toi/utils/constants.dart' as constants;
 import 'package:local_et_toi/utils/components/arrow_back.dart' as arrow_back;
