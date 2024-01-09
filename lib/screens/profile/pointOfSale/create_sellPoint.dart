779b1cc4ef59610226fd6bcae759d384b9d6e349
chachignot <unknown>
2024-01-09 16:39:03 +0100

    Feat (Screens) : Create the front for the screen to create a sellPoint
    Create the front for the screen to create a sellPoint, still need to link it to the backend.
    
    #46

--- lib/screens/profile/pointOfSale/create_sellPoint.dart
+++ lib/screens/profile/pointOfSale/create_sellPoint.dart
@@ -1,63 +1,193 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:local_et_toi/utils/buttons/drop_down_menu.dart';
+import 'package:local_et_toi/utils/constants.dart' as constants;
+import 'package:local_et_toi/utils/components/arrow_back.dart' as arrow_back;
+import 'package:local_et_toi/utils/textfields/textfields.dart';
+//import 'package:image_picker/image_picker.dart';
 
+import '../../../utils/buttons/buttons.dart';
 
-class create_sellpoint extends StatefulWidget {
-  const create_sellpoint({super.key});
+class SellPoint extends StatefulWidget {
+  const SellPoint({super.key});
 
   @override
-  _createSellpointState createState() => _createSellpointState();
+  _SellPointState createState() => _SellPointState();
 }
 
-class _createSellpointState extends State<create_sellpoint> {
-  final formKey = GlobalKey<FormState>();
-  late String identifiant, nom, prenom, email;
-
-  bool validateForm() {
-    if (formKey.currentState!.validate()) {
-      formKey.currentState!.save();
-      return true;
-    }
-    return false;
-  }
+class _SellPointState extends State<SellPoint> {
+  File? _image;
+  late String name, adresse, desc;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         clipBehavior: Clip.antiAlias,
-        decoration: const BoxDecoration(color: Color(0xFFFFFBE2)),
-        child: Stack(
-          children: [
-            Positioned(
-              left: 16,
-              top: 16,
-              child: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () {
-                  Navigator.of(context).maybePop();
-                },
+        decoration: const BoxDecoration(color: constants.beige),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                alignment : const FractionalOffset(0.01, 0.03),
+                child: arrow_back.ArrowBack(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
               ),
-            ),
-            const Positioned(
-              left: 0,
-              right: 0,
-              top: 39,
-              child: Center(
+
+              // Titre
+              const Padding(
+                padding: EdgeInsets.only(top: 45.0),
                 child: Text(
-                  'Inscription',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 24,
-                    fontFamily: 'Montserrat',
-                    fontWeight: FontWeight.w400,
-                    height: 0,
+                  "Ajout d'un point de vente",
+                  style: constants.titre,
+                ),
+              ),
+
+              // Image picker
+              GestureDetector(
+                //onTap: _getImage,
+                child: Container(
+                  margin: const EdgeInsets.all(20.0),
+                  width: 250.0,
+                  height: 250.0,
+                  decoration: BoxDecoration(
+                    color: constants.taupe,
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _image != null
+                          ? Image.file(_image!, fit: BoxFit.cover)
+                          : const Icon(
+                          Icons.add_a_photo, size: 50.0, color: Colors.white),
+                      const SizedBox(height: 8.0),
+                      const Text("Ajouter une photo pour votre point de vente",
+                          selectionColor: Color(0x00ffffff)),
+                    ],
                   ),
                 ),
               ),
-            ),
-          ],
+
+              // Formulaire
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Form(
+                  key: GlobalKey<FormState>(),
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 20.0),
+                        alignment: const FractionalOffset(0.5, 0.5),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Nom',
+                              style: constants.text,
+                            ),
+                            GreenTextFieldWithGreenerBorder(
+                              onSaved: (val) => name = val!,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Veuillez entrer un nom pour le point de vente';
+                                }
+                                return null;
+                              },
+                              keyboardType: TextInputType.text,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(bottom: 20.0),
+                        alignment: const FractionalOffset(0.5, 0.6),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Adresse',
+                              style: constants.text,
+                            ),
+                            GreenTextFieldWithGreenerBorder(
+                              onSaved: (val) => adresse = val!,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Veuillez entrer une adresse pour le point de vente';
+                                }
+                                return null;
+                              },
+                              keyboardType: TextInputType.number,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                          margin: const EdgeInsets.only(bottom: 20.0),
+                          alignment: const FractionalOffset(0.5, 0.7),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Description',
+                                style: constants.text,
+                              ),
+                              GreenTextFieldWithGreenerBorder(
+                                onSaved: (val) => desc = val!,
+                                validator: (value) {
+                                  return null;
+                                },
+                                keyboardType: TextInputType.text,
+                              ),
+                            ],
+                          )
+                      ),
+                      Container(
+                          margin: const EdgeInsets.only(bottom: 20.0),
+                          alignment: const FractionalOffset(0.5, 0.7),
+                          child: const Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Catégorie',
+                                style: constants.text,
+                              ),
+                              DropdownMenuWidget(
+                                itemList: constants.categoriesList,
+                              )
+                            ],
+                          )
+                      ),
+                      // Bouton Enregistrer
+                      GreenRoundedButton(
+                        onPressed: () {
+                          // si tous les champs sont remplis on enregistre
+                          if (Form.of(context)!.validate()) {
+                            // Enregistrer le produit
+                          }
+                        },
+                        buttonText: 'Enregistrer',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: SellPoint(),
+    ),
+  ));
 }
